53228899f441bc98b172c451c3842562ccd53acb
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2023-04-07 17:35:49 +0100

    files for Business Card

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Head from './Components/Head'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
+
+export default function App() {
+  return (
+    <div>
+      <Head />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
---
+++ src/Components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Main(){
+    return (
+        <div className='main'>
+            <div className='Interests'>
+                <h2 >About</h2>
+                <p>Hi, I'm Judith, a full stack engineer located in Edinburgh, Scotland. I have always been surrounded by tech-savvy people in my social environment; be it programmers, graphic designers or other IT support. In my previous roles I was the go-to person when colleagues needed help understanding their applications.</p>
+            </div>
+            <div className='Interests'>
+                <h2>Interests</h2>
+                <p>A football loving cycling enthusiast. Passionate baker and pug dog fan.</p>
+            </div>
+        </div>
+    )
+}
